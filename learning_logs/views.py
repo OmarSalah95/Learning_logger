327b02a77e290578303b43929db75c6e60d6a345
omzz wezee <omars.e.995@gmail.com>
2018-12-03 23:58:52 -0800

    Added new topic page however server has now crashed

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from .models import Topic
-
+from django.http import HttpResonseRedirect
+from django.core.urlresolvers import reverse
+from .forms import TopicForm 
 
 def index(request):
     """The home page of learning_log"""
@@ -18,3 +20,18 @@ def topic(request, topic_id):
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic' : topic, 'entries' : entries}
     return render(request, 'learning_logs/topics.html', context)
+
+def new_topic(request):
+    """Add new topics"""
+    if request.method != 'POST':
+        #No data submitted; create blank form
+        form = TopicForm()
+    else:
+        #POST data submitted; Process data.
+        form = TopicForm(data = request.POST)
+        if form.is_valid:
+            form.save()
+            return HttpResponseRedirect(reverse('learning_logs:topics'))
+    
+    context = {'form': form}
+    return render(request, 'learning_logs/new_topic.html', context)
